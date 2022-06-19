89376ef2aff3a25d5dd1b262605dc85dd80cf1a2
JoseDiazG <josethp@hotmail.com>
2022-06-19 17:49:10 -0500

    Instalando paquetes desde npm

--- 13-CourseNode2022/01-ConociendoPaquetesNode/app.js
+++ 13-CourseNode2022/01-ConociendoPaquetesNode/app.js
@@ -3,9 +3,16 @@
 
 //fs.appendFileSync('notes.txt','I live in Ecuador');
 
-const validator = require('validator');
-const getNotes = require('./notes.js');
+//const validator = require('validator');
+import chalk from 'chalk';
+const getNotes = require('./notes.js')
+
 const msg = getNotes();
 console.log(msg);
 
 console.log(validator.isEmail('josethp1@gmail.com'));
+
+const error = chalk.bold.red;
+console.log(error);
+
+console.log(chalk.blue('Hello world!'));

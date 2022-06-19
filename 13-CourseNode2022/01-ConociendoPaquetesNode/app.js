b51328a5848d55eb97e7b8bef26b754431e4489d
JoseDiazG <josethp@hotmail.com>
2022-06-19 17:17:29 -0500

    Importacion de archivos js en node

--- 13-CourseNode2022/01-ConociendoPaquetesNode/app.js
+++ 13-CourseNode2022/01-ConociendoPaquetesNode/app.js
@@ -1,4 +1,8 @@
-const fs = require('fs');
+//const fs = require('fs');
 //fs.writeFileSync('notes.txt', 'Mi nombre es Jose');
 
-fs.appendFileSync('notes.txt','I live in Ecuador');
+//fs.appendFileSync('notes.txt','I live in Ecuador');
+
+const getNotes = require('./notes.js');
+const msg = getNotes();
+console.log(msg);

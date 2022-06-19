b45076133c06b5b77e3f1b5405b8f77cb3d5e8be
JoseDiazG <josethp@hotmail.com>
2022-06-19 18:22:31 -0500

    arguemntos argv

---
+++ 13-CourseNode2022/02-AppConsola/app.js
@@ -0,0 +1,8 @@
+const command = process.argv[2];
+console.log(process.argv);
+
+if(command === 'add'){
+  console.log('Adding note!');
+}else if(command === 'remove'){
+  console.log('Removing note!');
+}

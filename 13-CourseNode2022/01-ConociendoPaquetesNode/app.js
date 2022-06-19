b73dd062dbb628f67a8cb25ab25d9ea71a2a77ae
JoseDiazG <josethp@hotmail.com>
2022-06-19 17:26:56 -0500

    Agregando npm instalando validator

--- 13-CourseNode2022/01-ConociendoPaquetesNode/app.js
+++ 13-CourseNode2022/01-ConociendoPaquetesNode/app.js
@@ -3,6 +3,9 @@
 
 //fs.appendFileSync('notes.txt','I live in Ecuador');
 
+const validator = require('validator');
 const getNotes = require('./notes.js');
 const msg = getNotes();
 console.log(msg);
+
+console.log(validator.isEmail('josethp1@gmail.com'));
